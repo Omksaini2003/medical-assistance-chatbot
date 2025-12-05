87fbd416284bad8272e51be3f1d7bab4f7981f0a
unknown <o.saini@iitg.ac.in>
2025-12-05 17:08:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-assistance-chatbot",
+    version="0.1.0",
+    author="Om kumar saini",
+    author_email="busi.saini2023@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
